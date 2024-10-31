54a8905cae9eab222d2e8f4728976c298efdff8c
quang-demo-fe <quang.le@glamscorp.jp>
2024-10-31 13:38:32 +0700

    Add project files

--- mini-project/src/App.tsx
+++ mini-project/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
-function App() {
+const App: React.FC = () =>  {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo:todo}]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <span className="heading">To-do-list</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd = {handleAdd}></InputField>
+      <TodoList todos={todos} setTodos={setTodos}></TodoList>
     </div>
   );
 }
