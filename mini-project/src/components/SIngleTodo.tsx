d836107afeb533f5b9c7ccac85b147ccb56b1ebb
quang-demo-fe <quang.le@glamscorp.jp>
2024-11-01 14:59:24 +0700

    add confirmation

--- mini-project/src/components/SIngleTodo.tsx
+++ mini-project/src/components/SIngleTodo.tsx
@@ -13,9 +13,19 @@ interface Props{
 const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
     const [edit, setEdit] = useState<boolean>(false);
     const [editTodo, setEditTodo] = useState<string>(todo.todo);
+    const [confirmId, setConfirmId] = useState<boolean>(false);
+
+    const handleDeleteClick = () => {
+        setConfirmId(true);
+    };
 
     const handleDelete = (id:number) => {
         setTodos(todos.filter((todo) => todo.id !== id));
+        setConfirmId(false);
+    };
+
+    const handleCancel = () => {
+        setConfirmId(false);
     };
 
     const handleEdit = (e:React.FormEvent, id:number) =>{
@@ -52,9 +62,18 @@ const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
                 >
                     <AiFillEdit></AiFillEdit>
                 </span>
-                <span className="icon" onClick={()=> handleDelete(todo.id)}>
+                <span className="icon_delete" onClick={()=> handleDeleteClick()}>
                     <AiFillDelete></AiFillDelete>
                 </span>
+                {confirmId === true && (
+                    <div className="todos_single--confirm">
+                        <span className="todos_single--text">Are you sure?</span>
+                        <span>
+                            <span className="todos_single--option" onClick={() => handleDelete(todo.id)}>Yes</span>
+                            <span className="todos_single--option" onClick={handleCancel}>No</span>      
+                        </span>
+                    </div>
+                    )}
             </div>
         </form>
     )
